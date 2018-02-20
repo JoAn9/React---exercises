093b20c6b0418ad55332d1f070a836af9700d2af
JoAn <joannacalinska@gmail.com>
2018-02-20 21:44:30 +0100

    update

--- 1_Zadania/Dzien_3/2_State_podsumowanie/js/zadanie01.jsx
+++ 1_Zadania/Dzien_3/2_State_podsumowanie/js/zadanie01.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class ClockTime extends React.Component {
+    render() {
+        return <h1>
+            {this.props.date.toLocaleTimeString()}
+        </h1>
+    }
+}
+
+class ClockDate extends React.Component {
+    render() {
+        return <h1>
+            {this.props.date.toLocaleDateString()}
+        </h1>
+    }
+}
+
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date()
+        }
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            this.setState({
+                date: new Date()
+            });
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        return <div>
+            <ClockTime date={this.state.date} />
+            <ClockDate date={this.state.date} />
+        </div>
+    }
+}
+
+
+class App extends React.Component {
+    render() {
+        return <Clock />
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function(){
     ReactDOM.render(
-        <h1>Hello, World!</h1>,
+        <App />,
         document.getElementById('app')
     );
 });
